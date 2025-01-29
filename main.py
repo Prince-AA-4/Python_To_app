4d2a06639c26dbbd4f4da53423ebe88b018c294b
Prince-AA-4 <abigiboozine175@gmail.com>
2025-01-29 11:00:25 +0000

    update: created an infinite loop

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 print("****Welcome to my to_do app****")
-user_option = input("Choose an option below: \n1.Add Task \n2.View \n3.Edit \n4.Delete ")
-
 
 todos=[]
-if user_option=="1":
-    # add a task
-    while True:
+while True:
+    user_option = input("Choose an option below: \n1.Add Task \n2.View \n3.Edit \n4.Delete \n5. Exit \n")
+    if user_option=="1":
+        # add a task
         todo =input("Enter to-do:\n")
         todos.append(todo) 
         print(f"{todo} added succesfully")
-if user_option=="2":
-    pass
-if user_option=="3":
-    pass
-if user_option=="4":
-    pass
-else:
-    print("wrong option")
+    elif user_option=="2":
+        pass
+    elif user_option=="3":
+        pass
+    elif user_option=="4":
+        pass
+    elif user_option == "5":
+        print("Goodbye....")
+        exit()
+    else:
+        print("wrong option")
